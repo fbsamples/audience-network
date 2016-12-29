f3b6eaa65996838d866f067eaff558d2abff71f5
Yun Peng Wang <ypwang@fb.com>
2016-12-29 13:03:26 -0800

    Update latest Audience Network SDK

--- NativeAd/ViewController.h
+++ NativeAd/ViewController.h
@@ -8,7 +8,22 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@import FBAudienceNetwork;
+
+@interface ViewController : UIViewController  <FBNativeAdDelegate>
+
+@property (strong, nonatomic) IBOutlet UILabel *adStatusLabel;
+
+@property (strong, nonatomic) IBOutlet UIImageView *adIconImageView;
+@property (weak, nonatomic) IBOutlet FBMediaView *adCoverMediaView;
+@property (strong, nonatomic) IBOutlet UILabel *adTitleLabel;
+@property (strong, nonatomic) IBOutlet UILabel *adBodyLabel;
+@property (strong, nonatomic) IBOutlet UIButton *adCallToActionButton;
+@property (strong, nonatomic) IBOutlet UILabel *adSocialContextLabel;
+@property (strong, nonatomic) IBOutlet UILabel *sponsoredLabel;
+@property (weak, nonatomic) IBOutlet FBAdChoicesView *adChoicesView;
+
+@property (strong, nonatomic) IBOutlet UIView *adUIView;
 
 
 @end
